503d2090b11245fa534f5eb83a2935e4bdc05291
olangoisack <141493348+olangoisack@users.noreply.github.com>
2023-09-20 18:30:03 +0300

    Add files via upload

---
+++ Standard Deviation Implementation.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import numpy as np
+
+
+# In[2]:
+
+
+#Data preparation
+
+
+# In[3]:
+
+
+ll=[[1,2,3,4,5,6],[3,4,5,6,7,8]]
+
+
+# In[4]:
+
+
+np.std(ll)
+
+
+# In[ ]:
+
+
+
+
