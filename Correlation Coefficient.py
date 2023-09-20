19917b79a6169b2a443e197a4759d1ada9b22981
olangoisack <141493348+olangoisack@users.noreply.github.com>
2023-09-20 18:24:51 +0300

    Add files via upload

---
+++ Correlation Coefficient.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import numpy as np
+
+
+# In[2]:
+
+
+#Data Preparation
+
+
+# In[3]:
+
+
+vc=[1,2,39,0,8]
+vb=[1,2,38,0,8]
+
+
+# In[4]:
+
+
+#Function-based implementation:
+
+
+# In[5]:
+
+
+np.corrcoef(vc,vb)
+
+
+# In[ ]:
+
+
+
+
