5455de6372eed5695c924980907c183a6f537c73
olangoisack <141493348+olangoisack@users.noreply.github.com>
2023-09-20 18:30:48 +0300

    Add files via upload

---
+++ Covariance implementation.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[5]:
+
+
+import numpy as np
+
+
+# In[6]:
+
+
+#Data preparation
+
+
+# In[7]:
+
+
+x = np.array([[1,2],[3,7]])
+
+
+# In[8]:
+
+
+print(np.cov(x))
+
+
+# In[ ]:
+
+
+
+
